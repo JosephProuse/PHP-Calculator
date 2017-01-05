1a3b454ede4c109bce92b3a55556e41d27830ed2
ProNoXin <joseph.prouse17@gmail.com>
2017-01-05 14:28:13 +0000

    More keypad code

--- Calculator.php
+++ Calculator.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
-
 <!-- 
 Author: Pronoxin
 Filename: Calculator.php
 Description: A simple PHP calculator
 Creation: 14/12/2016
 
-Updated: 04/01/2017
-Reason: Started adding a keypad as another form to the program
+Updated: 05/01/2017
+Reason: More keypad code
 -->
-
 <head>
 	<meta charset="utf-8">
 	<link href="style.css" rel="stylesheet">
@@ -26,8 +24,27 @@ Reason: Started adding a keypad as another form to the program
 			document.getElementById("Vtext").style.display="none";
 			document.getElementById("text").style.display="block";
 			document.getElementById("Vclick").style.display="block";
+		}	
+	}
+	
+	var number = "";
+	var operator = "";
+	function addnumber(num){
+		number = number + num;
+		return number;
+	}
+	
+	function defineoperator(opp){
+		if (opp=="+"){
+			opeprator = ;
 		}
-			
+	}
+	
+	function clear(){
+		var number = "";
+		var operator = "";
+		return number;
+		return operator;
 	}
 	</script>
 </head>
@@ -35,27 +52,31 @@ Reason: Started adding a keypad as another form to the program
 <body id="calculator" onload="visability(2)">
 	<button onclick="visability(1)" class="formatting" id="Vclick">Change format</button>
 	<button onclick="visability(2)" class="formatting" id="Vtext">Change format</button>
-	<form id="click" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+	<form name="Keypad" id="click" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
 	<table id="keypad" cellpadding="5" cellspacing="3">
 	<tr>
-    	<td onclick="addCode('1');">1</td>
-        <td onclick="addCode('2');">2</td>
-        <td onclick="addCode('3');">3</td>
+    	<td onclick="addnumber(1)">1</td>
+        <td onclick="addnumber(2)">2</td>
+        <td onclick="addnumber(3)">3</td>
+		<td onclick="addnumber(+)">+</td>
     </tr>
     <tr>
-    	<td onclick="addCode('4');">4</td>
-        <td onclick="addCode('5');">5</td>
-        <td onclick="addCode('6');">6</td>
+    	<td onclick="addnumber(4)">4</td>
+        <td onclick="addnumber(5)">5</td>
+        <td onclick="addnumber(6)">6</td>
+		<td onclick="addnumber(-)">-</td>
     </tr>
     <tr>
-    	<td onclick="addCode('7');">7</td>
-        <td onclick="addCode('8');">8</td>
-        <td onclick="addCode('9');">9</td>
+    	<td onclick="addnumber(7)">7</td>
+        <td onclick="addnumber(8)">8</td>
+        <td onclick="addnumber(9)">9</td>
+		<td onclick="addnumber(*)">*</td>
     </tr>
     <tr>
     	<td>#</td>
-        <td onclick="addCode('0');">0</td>
-        <td onclick="clear()">C</td>
+        <td onclick="addnumber(0)">0</td>
+        <td onclick="clear">C</td>
+		<td onclick="addnumber(/)">/</td>
     </tr>
 	</table>
 	<input class="calculate" name="sendC" type="submit" value="Calculate">
@@ -75,36 +96,37 @@ function calculator(){
 	$first = $_POST['Number1'];
 	$second = $_POST['Number2'];
 	
+	
 	if($first==""){
 		echo "<script type='text/javascript'>alert('please enter the first number');</script>";
 		return false;
 	}
+	if ($second==""){
+		echo "<script type='text/javascript'>alert('please enter the second number');</script>";
+		return false;
+	}	
 	if($operator==""){
 		echo "<script type='text/javascript'>alert('please enter an operator');</script>";
 		return false;
-	}
-	if ($second==""){
-		echo "<script type='text/javascript'>alert('please enter the second number');</script>";
+	}	
+	if($operator!="+" && $operator!="-" && $operator!="*" && $operator!="/"){
+		echo "<script type='text/javascript'>alert('please enter a valid operator');</script>";
 		return false;
 	}
 	if($operator=="+"){
 		echo "$first + $second = ".$first + $second. " <br>";
 	}
 	if($operator=="-"){
-		echo " $first - $second = " .$first - $second. " <br>";
+		echo "$first - $second = " .$first - $second. " <br>";
 	}
 	if($operator=="/"){
-		echo " $first / $second = " .$first / $second. " <br>";
+		echo "$first / $second = " .$first / $second. " <br>";
 	}
 	if($operator=="*"){
 		echo $first * $second , "<br>";
 		for($x=1; $x<=$second; $x++){
 			echo " $first x $x = " .$first * $x." <br>";
 		}
-	}
-	if($operator!="+" && $operator!="-" && $operator!="*" && $operator!="/"){
-		echo "<script type='text/javascript'>alert('please enter a valid operator');</script>";
-		return false;
 	}
 }
 //runs the function when the submit button is pressed
